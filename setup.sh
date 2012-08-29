edadddfb93762d6385f08ed608b6a0a377f9e559
Xing Zhou <xing@xingzhou.me>
2012-08-29 16:04:29 -0400

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+ln -s $(pwd)/bash/bash_profile $HOME/.bash_profile
+ln -s $(pwd)/bash/bashrc $HOME/.bashrc
+ln -s $(pwd)/bash/profile $HOME/.profile
+
+ln -s $(pwd)/git/gitconfig $HOME/.gitconfig
+
+ln -s $(pwd)/vim-files/vim $HOME/.vim
+ln -s $(pwd)/vim-files/vim/vimrc $HOME/.vimrc
+
+ln -s $(pwd)/zsh/zsh $HOME/.zsh
+ln -s $(pwd)/zsh/zshrc $HOME/.zshrc
