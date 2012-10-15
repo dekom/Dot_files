6b5fcbb007d62dee2c0c4e2141a600e72fdf43db
Xing Zhou <xing@xingzhou.me>
2012-10-14 23:00:17 -0400

    Reorganize vim config folder

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ ln -sin $(pwd)/bash/profile $HOME/.profile
 ln -sin $(pwd)/git/gitconfig $HOME/.gitconfig
 ln -sin $(pwd)/git/gitignore $HOME/.gitignore
 
-ln -sin $(pwd)/vim-files/vim $HOME/.vim
-ln -sin $(pwd)/vim-files/vim/vimrc $HOME/.vimrc
+ln -sin $(pwd)/vim $HOME/.vim
+ln -sin $(pwd)/vim/vimrc $HOME/.vimrc
 
 ln -sin $(pwd)/zsh/zsh $HOME/.zsh
 ln -sin $(pwd)/zsh/zshrc $HOME/.zshrc
